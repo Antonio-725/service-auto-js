123da4bcb3897d5514c48420ac0d518a97f5a5a4
AntoMutua <amuimi@onfonmobile.com>
2025-07-18 17:48:58 +0300

    Changed CORS

--- vehicle-service-server/index.js
+++ vehicle-service-server/index.js
@@ -3,10 +3,16 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
+
+// Fallback to localhost if CLIENT_URL is not set
+const allowedOrigin = process.env.CLIENT_URL || "http://localhost:5173";
+
+
 // CORS Middleware
 const cors = require("cors");
 app.use(cors({
-  origin: "http://localhost:5173", // Allow only your React app
+  origin: allowedOrigin,
+  //origin: "http://localhost:5173", // Allow only your React app
   credentials: true, // If you're using cookies
 }));
 
