e64183ec4d166b7fef8401011065592d196040c9
AntoMutua <amuimi@onfonmobile.com>
2025-07-18 18:29:46 +0300

    Make CORS origin dynamic via env

--- vehicle-service-server/index.js
+++ vehicle-service-server/index.js
@@ -7,7 +7,12 @@ const PORT = process.env.PORT || 5000;
 // Fallback to localhost if CLIENT_URL is not set
 // const allowedOrigin = process.env.CLIENT_URL || "http://localhost:5173";
 
-const allowedOrigin = process.env.CLIENT_URL || "https://service-auto-js.vercel.app";
+//const allowedOrigin = process.env.CLIENT_URL || "https://service-auto-js.vercel.app";
+
+const allowedOrigin = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL || "https://service-auto-js.vercel.app"
+];
 
 
 // CORS Middleware
